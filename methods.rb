9e6c0a524d7c7a167aad3d70dbdae6a85138abd1
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-17 13:10:59 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,71 @@
 # Your code here!
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+#   end
+
+#  puts my_method (6)
+
+
+
+#  def say_hi(name)
+#   puts "Hi there, #{name}!"
+# end
+
+# puts say_hi ("mahir")
+
+
+#  def add_and_log(num1, num2)
+#   puts num1 + num2
+#  end
+
+#   sum1 = add_and_log(2, 2)
+
+ 
+
+   def greet_programmer
+      puts "Hello, programmer!"
+   end
+
+ puts greet_programmer
+
+
+
+   def greet (name)
+    puts "Hello, #{name}!"
+ end
+
+ puts greet 'Naureen'
+ puts greet 'Jimmy'
+
+
+
+
+ def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+ end
+ 
+puts greet_with_default
+
+
+  def add(num1, num2)
+     puts num1 + num2
+     num1 + num2
+   end
+
+  puts add(2, 5)
+
+
+
+
+  def halve (number)
+    if number.class != Integer
+      return nil
+    end
+
+     number / 2
+  end
+
+
+  
